48ea171e3fb704b4492e1cab9391bce829685001
CarlosFonte <fontenarciso@gmail.com>
2024-01-25 13:53:50 +0100

    holaMundoJS01

---
+++ 01-tipos/01-variables.js
@@ -0,0 +1,23 @@
+//Creamos variable. 
+//Definir una variable es darle un nombre a la variable (let nombre)
+let nombre = 'Hola mundo';
+
+//Modos de nombrar varibles (conveciones).
+//UpperCamelCase. Esto suele usarse en algunos lenguajes para nombrar constantes (const).
+let NombreCompleto;
+
+//camelCase. Esta es la manera más usada para las variables en JS.
+let nombreCompleto;
+
+//snake_case.
+let nombre_completo;
+
+console.log(nombre);
+
+//Podemos definir una variable y no darle valor.
+//Definimos variable
+let apellido;
+//Añadimos valor a la variable.
+apellido = 'Fonte';
+//Podemos definir más de una variable a la vez.
+let calle, numero, piso;
---
+++ 01-tipos/02-primitivos.js
@@ -0,0 +1,17 @@
+//Datos primitivos.
+//Number
+let numero = 1;
+
+//Strings
+let texto = "Hola mundo";
+
+//Booleans
+let verdadero = true;
+let falso = false;
+
+//Undefined
+let noDefinido;
+let undef = undefined; //undefined puede ser dos cosas en JS: 1.- Tipo de dato. 2.- Valor de variable.
+
+//Null
+let nulo = null; //esto debemos usarlo cuando queremos que una variable tenga un valor, pero queremos que es valor sea nulo.
